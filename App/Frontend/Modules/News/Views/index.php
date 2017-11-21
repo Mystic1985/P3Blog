f976ef8435095c7722bb5b762ccaa17760e429a6
Mystic1985 <arnaud.griess@gmail.com>
2017-11-21 12:19:00 +0100

    Intégration des boutons bootstrap pour aller aux pages précédentes et suivantes

--- App/Frontend/Modules/News/Views/index.php
+++ App/Frontend/Modules/News/Views/index.php
@@ -3,8 +3,9 @@
 foreach ($listeNews as $news)
 {
 ?>
-  <h2><a href="news-<?= $news['id'] ?>.html"><?= $news['titre'] ?></a></h2>
+  <h2 class="newstitle"><a href="news-<?= $news['id'] ?>.html"><?= $news['titre'] ?></a></h2>
   <p><?= nl2br($news['contenu']) ?></p>
 <?php
 }
 ?>
+<a class="btn btn-default btn-xs" href="/"><span class="glyphicon glyphicon-fast-backward" aria-hidden="true"></span></a> <a class="btn btn-default btn-xs" href="page-<?php echo $pagePrecedente ?>.html"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span></a> <strong><?php echo $page ?></strong> <a class="btn btn-default btn-xs" href="page-<?php echo $pageSuivante ?>.html"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></a>  <a class="btn btn-default btn-xs" href="page-<?php echo $lastPage ?>.html"><span class="glyphicon glyphicon-fast-forward" aria-hidden="true"></span></a>
