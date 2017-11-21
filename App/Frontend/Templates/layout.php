8091be1976503b5bb250cea37921b8728de7142d
Mystic1985 <arnaud.griess@gmail.com>
2017-11-21 09:01:12 +0100

    Désactivation de TinyMCE dans le layout du front

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -14,11 +14,6 @@
     <script src="js/jquery.js"></script>
     <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script>
-    tinymce.init({
-    selector: 'textarea'
-  });
-  </script>
   </head>
  
   <body >
